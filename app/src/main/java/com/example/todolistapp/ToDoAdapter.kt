7fab3c27ce9a9f76f7b236451a7ef807a30c6339
Ivan <thebrianmorgan0@gmail.com>
2021-05-22 23:49:17 +0300

    without bindfunction

---
+++ app/src/main/java/com/example/todolistapp/ToDoAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.todolistapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ToDoAdapter(
+    val todos:MutableList<ToDo>
+) : RecyclerView.Adapter<ToDoAdapter.ToDoViewHolder>() {
+    class ToDoViewHolder(itemView:View): RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoViewHolder {
+        return ToDoViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_todo,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ToDoViewHolder, position: Int) {
+        
+    }
+
+    override fun getItemCount(): Int {
+        return todos.size
+    }
+}
